4c5b88780b1ffc3b779e13a4f9ebaf761bd52823
mghadieh <mohamadghadieh@gmail.com>
2022-10-13 09:50:48 -0400

    Finished NestedLists ex.

--- HackerRank/5_NestedLists.py
+++ HackerRank/5_NestedLists.py
@@ -1,9 +1,9 @@
 # Given the names and grades for each student in a Physics class of students, store them in a nested list
 # and print the name(s) of any student(s) having the second lowest grade.
-# 
+#
 # Note: If there are multiple students with the same grade, order their names alphabetically and print each
 # name on a new line.
-# 
+#
 # Input Format
 # The first line contains an integer, N , the number of students.
 # The 2N subsequent lines describe each student over 2 lines; the first line contains a student's name, and
@@ -18,7 +18,7 @@
 # students, order their names alphabetically and print each one on a new line.
 #
 # Sample Input 0
-#----------------
+# ----------------
 # 5
 # Harry
 # 37.21
@@ -30,14 +30,14 @@
 # 41
 # Harsh
 # 39
-# 
+#
 # Sample Output 0
-#-----------------
+# -----------------
 # Berry
 # Harry
 #
 # Explanation 0
-#---------------
+# ---------------
 # There are students in this class whose names and grades are assembled to build the following list:
 # python students = [['Harry', 37.21], ['Berry', 37.21], ['Tina', 37.2], ['Akriti', 41], ['Harsh', 39]]
 # The lowest grade of belongs to
@@ -45,17 +45,23 @@
 # Harry andBerry, so we order their names alphabetically and print each name on a new line.
 
 
+nestedList = []
+scores = []
+names = []
 if __name__ == '__main__':
-    lst = []
-    sublist = []
-
     for _ in range(int(input())):
         name = input()
         score = float(input())
-        sublist.append(name)
-        sublist.append(score)
-        print(sublist)
-        lst.extend(sublist)
-        sublist = []
+        nestedList.append([name, score])
+        scores.append(score)
+    set_scores = set(scores)
+    lst_scores = list(set_scores)
+    lst_scores.sort()
+    second = lst_scores[1]
 
-    print(lst)
+    for item in nestedList:
+        if second in item:
+            names.append(item[0])
+    names.sort()
+    for name in names:
+        print(name)
